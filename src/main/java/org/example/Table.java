993cd8e0a546d389af9b08827883833cdd919c01
dudkiewicz.mikolaj@gmail.com <dudkiewicz.mikolaj@gmail.com>
2024-10-28 08:44:00 +0100

    added some comments to Table.java

--- src/main/java/org/example/Table.java
+++ src/main/java/org/example/Table.java
@@ -1,5 +1,8 @@
 /**
- * TODO: make it impossible to create instance od Player, Deck or Card outside of the Table
+ * TODO:
+ * make it impossible to create instance of Player, Deck or Card outside of the Table
+ * make an easier way to introduce players in constructor
+ * limit amount of players to 10
  */
 
 package org.example;
@@ -8,10 +11,18 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class Table {
+    /**
+     * Table represents a table of players which includes given attributes:
+     * tablePlayers : Set<Player>, representing players at the table
+     * tableDeck : Deck, represents deck assigned to the table
+     */
     private final Set<Player> tablePlayers;
     private final Deck tableDeck;
 
     public Table(Set<String> playerNames) {
+        /**
+         * constructor automatically shuffles deck of cards
+         */
         this.tableDeck = Deck.createSortedDeck();
         this.tableDeck.shuffle();
         this.tablePlayers = new LinkedHashSet<>();
